07a7ae9398a1c93fdc362cf7f27eced1f2d8498a
elepot87 <78643839+elepot87@users.noreply.github.com>
2021-10-22 17:18:05 +0200

    add version with class List add/remove instead of style

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ const btnMenu = document.querySelector(".fa-bars");
 // Scatenare evento al click sull'icona del menù hamburger, ovvero mostrarlo
 btnMenu.addEventListener("click", function () {
   const hamburgerMenuOpen = document.querySelector(".hamburger-menu");
-  hamburgerMenuOpen.style.display = "block";
+  // hamburgerMenuOpen.style.display = "block";
+
+  // Variante con l'aggiunta della classe richiamata dal CSS
+  hamburgerMenuOpen.classList.add("active");
 });
 
 // Nascondere menù hamburger al click sulla X
@@ -14,5 +17,8 @@ const btnClose = document.querySelector(".fa-times");
 
 btnClose.addEventListener("click", function () {
   const hamburgerMenuClosed = document.querySelector(".hamburger-menu");
-  hamburgerMenuClosed.style.display = "none";
+  // hamburgerMenuClosed.style.display = "none";
+
+  // Variante con eliminazione della classe del CSS
+  hamburgerMenuClosed.classList.remove("active");
 });
